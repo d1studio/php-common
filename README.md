9ebded8f050b31883dddafaaf2191b3290433c43
黑猫 <ct124202@alibaba-inc.com>
2017-05-15 11:35:48 +0800

    添加备注信息，并且添加文件助手类

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # php-helper
 
 *php-helper是一个工具包*
+
+安装方式
+
+    composer require d1studio/php-helper
---
+++ src/File.php
@@ -0,0 +1,13 @@
+<?php
+namespace d1studio\php_helper;
+
+class File{
+    /**
+     * 向文件追加一行的方法，需要锁定文件并且追加内容
+     * @param $file_name string 文件名
+     * @param $line      string 追加数据
+     */
+    public static function appendLine($file_name,$line){
+        file_put_contents($file_name, $line, FILE_APPEND | LOCK_EX);
+    }
+}
